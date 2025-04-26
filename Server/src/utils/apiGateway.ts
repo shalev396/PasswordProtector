1828caf19df66ca612354f5d84979d359fd6f9e2
shalev396 <shalev396@gmail.com>
2025-04-26 19:16:33 +0300

    swap to httpAPI

--- Server/src/utils/apiGateway.ts
+++ Server/src/utils/apiGateway.ts
@@ -21,7 +21,6 @@ export const formatJSONResponse = (
     statusCode,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
       "Content-Type": "application/json",
     },
     body: JSON.stringify(response),
