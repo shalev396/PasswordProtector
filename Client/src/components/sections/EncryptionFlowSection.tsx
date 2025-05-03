26e8e0ec0b2f7fd8162f283287ca0264f11ada43
shalev396 <shalev396@gmail.com>
2025-05-03 22:44:31 +0300

    fix for prod

--- Client/src/components/sections/EncryptionFlowSection.tsx
+++ Client/src/components/sections/EncryptionFlowSection.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { ArrowRight, Lock, Key, Database } from "lucide-react";
+// import { Link } from "react-router-dom";
+import {
+  //ArrowRight,
+  Lock,
+  Key,
+  Database,
+} from "lucide-react";
 import { motion } from "framer-motion";
 
 const EncryptionFlowSection: React.FC = () => {
